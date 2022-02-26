ff62dba9561a66a21e024f6e9c6a6656d2e89d17
prasia <92187457+prasia@users.noreply.github.com>
2022-02-26 13:56:45 -0800

    Add files via upload

---
+++ training.py
@@ -0,0 +1,28 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer
+
+intents = json.loads(open("intents.json").read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ["?", "!", ".", ","]
+
+for intent in intents['intents']:
+  for pattern in intent["patterns"]:
+    word_list = nltk.tokenize(pattern) #splits text into words
+    words.append(word_list)
+    documents.append((word_list, intent['tag']))
+
+print(documents)
